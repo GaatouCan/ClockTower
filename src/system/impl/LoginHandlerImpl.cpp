9584dd399a4533785e388d49f82e1ec2a0b29668
Gaatou <1752651025@qq.com>
2024-10-15 23:04:24 +0800

    Initial commit

--- src/system/impl/LoginHandlerImpl.cpp
+++ src/system/impl/LoginHandlerImpl.cpp
@@ -1,9 +1,21 @@
 #include "LoginHandlerImpl.h"
+#include "../../protobuf/ProtoType.generated.h"
+#include <login.pb.h>
 
 namespace base {
-    asio::awaitable<void> LoginHandlerImpl::login(const std::shared_ptr<Connection> &conn, const LoginInfo &) {
+    asio::awaitable<void> LoginHandlerImpl::login(const std::shared_ptr<Connection> &conn, const LoginInfo &info) {
     }
 
-    LoginInfo LoginHandlerImpl::parseLoginInfoT(Package *) {
+    LoginInfo LoginHandlerImpl::parseLoginInfoT(Package *pkg) {
+        if (pkg->id() != static_cast<uint32_t>(protocol::ProtoType::CS_Login_Request))
+            return {};
+
+        Login::CS_Login_Request request;
+        request.ParseFromString(pkg->data());
+
+        return {
+            request.player_id(),
+            request.token()
+        };
     }
 } // base

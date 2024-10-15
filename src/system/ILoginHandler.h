85aaf37634fef04d4532152b8d83dacdffbf681e
admin <xxx@xx.xom>
2024-10-15 19:47:41 +0800

    Initial commit

--- src/system/ILoginHandler.h
+++ src/system/ILoginHandler.h
@@ -1,7 +1,30 @@
 #pragma once
 
-namespace base {
-    class ILoginHandler {
+#include <asio.hpp>
+#include "../base/IPackage.h"
 
+namespace base {
+
+    struct LoginInfo {
+        uint64_t pid;
+        std::string token;
+    };
+
+    class ILoginHandler {
+    public:
+        virtual ~ILoginHandler() = default;
+
+        virtual LoginInfo parseLoginInfo(IPackage *) = 0;
+        virtual asio::awaitable<void> login(const std::shared_ptr<class Connection> &conn, LoginInfo &) = 0;
+    };
+
+    template<PACKAGE_TYPE T>
+    class TLoginHandler : public ILoginHandler {
+    public:
+        LoginInfo parseLoginInfo(IPackage *pkg) override {
+            return parseLoginInfoT(dynamic_cast<T *>(pkg));
+        }
+
+        virtual LoginInfo parseLoginInfoT(T *) = 0;
     };
 }

dde3476390e130b0bc18c7fae86f7bd8147a4ebf
admin <xxx@xx.xom>
2024-10-17 16:29:46 +0800

    Initial commit

--- src/server.cpp
+++ src/server.cpp
@@ -1,7 +1,11 @@
 #include <spdlog/spdlog.h>
+#include "base/GameWorld.h"
 
 auto main(int argc, char *argv[]) -> int {
     spdlog::info("Hello World!");
+
+    GetWorld().Init();
+    GetWorld().Run();
 
     return 0;
 }

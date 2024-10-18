11eac9e91293754af601e16f957b150435b1b866
admin <xxx@xx.xom>
2024-10-18 12:02:16 +0800

    Initial commit

--- src/system/database/IDBTable.h
+++ src/system/database/IDBTable.h
@@ -6,7 +6,7 @@ class IDBTable {
 public:
     virtual ~IDBTable() = default;
 
-    virtual void Query(mysqlx::Schema &schema) = 0;
+    virtual mysqlx::RowResult Query(mysqlx::Schema &schema) = 0;
     virtual void Read(mysqlx::Row &row) = 0;
     virtual void Write(mysqlx::Schema &schema) = 0;
     virtual void Remove(mysqlx::Schema &schema) = 0;

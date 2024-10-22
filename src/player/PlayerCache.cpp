02cea3d42127ebb0fe9e50e50f80fb708c431d7a
Gaatou <1752651025@qq.com>
2024-10-23 02:21:02 +0800

    Initial commit

---
+++ src/player/PlayerCache.cpp
@@ -0,0 +1,10 @@
+#include "PlayerCache.h"
+
+PlayerCache::PlayerCache(asio::io_context &ctx) : IManager(ctx) {
+}
+
+PlayerCache::~PlayerCache() {
+}
+
+void PlayerCache::Init() {
+}
---
+++ src/player/PlayerCache.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "../system/manager/IManager.h"
+
+class PlayerCache final : public base::IManager {
+
+public:
+    explicit PlayerCache(asio::io_context &ctx);
+    ~PlayerCache() override;
+
+    void Init() override;
+};

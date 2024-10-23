7e963779e6dcfc76674026430e4e388e3378f42f
Gaatou <1752651025@qq.com>
2024-10-24 00:59:41 +0800

    Initial commit

--- src/player/PlayerCache.cpp
+++ src/player/PlayerCache.cpp
@@ -4,4 +4,22 @@ PlayerCache::PlayerCache(asio::io_context &ctx) : IManager(ctx) {
 }
 
 PlayerCache::~PlayerCache() {
+
+}
+
+void PlayerCache::OnTick(TimePoint now) {
+    // TODO
+}
+
+void PlayerCache::SyncCache(CacheNode node) {
+    std::unique_lock lock(blockMutex_);
+    cacheMap_.insert_or_assign(node.pid, node);
+}
+
+std::optional<CacheNode> PlayerCache::FindCacheNode(const uint64_t pid) const {
+    std::shared_lock lock(sharedMutex_);
+    if (const auto iter = cacheMap_.find(pid); iter != cacheMap_.end()) {
+        return iter->second;
+    }
+    return std::nullopt;
 }
--- src/player/PlayerCache.h
+++ src/player/PlayerCache.h
@@ -3,11 +3,21 @@
 #include "../system/manager/IManager.h"
 #include "CacheNode.h"
 
+#include <mutex>
+#include <shared_mutex>
+
 class PlayerCache final : public base::IManager {
 
     std::unordered_map<uint64_t, CacheNode> cacheMap_;
+    std::mutex blockMutex_;
+    mutable std::shared_mutex sharedMutex_;
 
 public:
     explicit PlayerCache(asio::io_context &ctx);
     ~PlayerCache() override;
+
+    void OnTick(TimePoint now) override;
+
+    void SyncCache(CacheNode node);
+    std::optional<CacheNode> FindCacheNode(uint64_t pid) const;
 };

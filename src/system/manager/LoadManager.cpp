177277e9db31e6f674d471a532afd76ab6336805
admin <xxx@xx.xom>
2024-10-23 10:21:39 +0800

    Initial commit

--- src/system/manager/LoadManager.cpp
+++ src/system/manager/LoadManager.cpp
@@ -1,9 +1,11 @@
 #include "ManagerSystem.h"
 
 #include "../../player/PlayerManager.h"
+#include "../../player/PlayerCache.h"
 
 namespace base {
     void ManagerSystem::LoadManager() {
         REGISTER_MANAGER(PlayerManager)
+        REGISTER_MANAGER(PlayerCache)
     }
 }

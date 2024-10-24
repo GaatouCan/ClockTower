c893b108dad9984c8e48e791ca50952e523d05cd
admin <xxx@xx.xom>
2024-10-24 11:51:37 +0800

    Initial commit

--- src/player/PlayerCache.cpp
+++ src/player/PlayerCache.cpp
@@ -11,15 +11,18 @@ void PlayerCache::OnTick(TimePoint now) {
     // TODO
 }
 
-void PlayerCache::SyncCache(CacheNode node) {
+void PlayerCache::SyncCache(CacheNode *node) {
+    if (node == nullptr)
+        return;
+
     std::unique_lock lock(blockMutex_);
-    cacheMap_.insert_or_assign(node.pid, node);
+    cacheMap_.insert_or_assign(node->pid, node);
 }
 
-std::optional<CacheNode> PlayerCache::FindCacheNode(const uint64_t pid) const {
+const CacheNode *PlayerCache::FindCacheNode(const uint64_t pid) const {
     std::shared_lock lock(sharedMutex_);
     if (const auto iter = cacheMap_.find(pid); iter != cacheMap_.end()) {
         return iter->second;
     }
-    return std::nullopt;
+    return nullptr;
 }

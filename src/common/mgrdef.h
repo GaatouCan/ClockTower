503593ee6ccb8e75c6114c839cc07803fb5f8d19
admin <xxx@xx.xom>
2024-10-30 17:05:21 +0800

    Initial commit

---
+++ src/common/mgrdef.h
@@ -0,0 +1,8 @@
+//
+// Created by admin on 24-10-30.
+//
+
+#ifndef MGRDEF_H
+#define MGRDEF_H
+
+#endif //MGRDEF_H
---
+++ src/common/pkgdef.h
@@ -0,0 +1,8 @@
+//
+// Created by admin on 24-10-30.
+//
+
+#ifndef PKGDEF_H
+#define PKGDEF_H
+
+#endif //PKGDEF_H

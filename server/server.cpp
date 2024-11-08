a57679962995c587a8774bab76547ff446873cbd
admin <xxx@xx.xom>
2024-11-08 10:50:46 +0800

    Initial commit

--- server/server.cpp
+++ server/server.cpp
@@ -2,6 +2,14 @@
 
 #include "../base/GameWorld.h"
 #include "../base/system/config/ConfigSystem.h"
+#include "../base/system/protocol/ProtocolSystem.h"
+#include "../base/system/manager/ManagerSystem.h"
+
+#include "impl/ConnectionHandlerImpl.h"
+
+#include "common/proto_def.h"
+#include "common/mgr_def.h"
+
 
 auto main(int argc, char *argv[]) -> int {
     spdlog::set_level(spdlog::level::trace);
@@ -10,6 +18,20 @@ auto main(int argc, char *argv[]) -> int {
     if (const auto sys = GetSystem<UConfigSystem>(); sys != nullptr) {
         sys->SetYAMLPath("../../config");
     }
+
+    if (const auto sys = GetSystem<UProtocolSystem>(); sys != nullptr) {
+        LoadProtocol(sys);
+    }
+
+    if (const auto sys = GetSystem<UManagerSystem>(); sys != nullptr) {
+        sys->SetManagerLoader(&LoadManager);
+    }
+
+    GetWorld().FilterConnection([](const AConnectionPointer &conn) {
+        if (conn != nullptr) {
+            conn->SetHandler<UConnectionHandlerImpl>();
+        }
+    });
 
     GetWorld().Init();
     GetWorld().Run();

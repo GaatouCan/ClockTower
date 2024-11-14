45b11360d8146ad3b2bd9faaf95946cf5bf51db5
admin <xxx@xx.xom>
2024-11-14 09:47:14 +0800

    Initial commit

--- server/common/logger_def.h
+++ server/common/logger_def.h
@@ -6,22 +6,22 @@
 
 static constexpr auto kLogPath = "./logs/";
 
-#define REGISTER_FILE_LOGGER(loggerName, fileName)\
-{ \
-    const auto fileSink = std::make_shared<spdlog::sinks::daily_file_sink_mt>(kLogPath + std::string(fileName), 2, 30); \
-    fileSink->set_level(spdlog::level::trace); \
-    std::vector<spdlog::sink_ptr> sinks{consoleSink, fileSink};  \
-    const auto logger = std::make_shared<spdlog::logger>(loggerName, sinks.begin(), sinks.end()); \
-    logger->set_level(spdlog::level::trace); \
-    register_logger(logger); \
-}
+inline void CreateDefaultLogger(const spdlog::sink_ptr &consoleSink, const std::string &logName, const std::string &path) {
+    const auto fileSink = std::make_shared<spdlog::sinks::daily_file_sink_mt>("./logs/" + path, 2, 30);
+    fileSink->set_level(spdlog::level::trace);
 
+    std::vector<spdlog::sink_ptr> sinks{consoleSink, fileSink};
+
+    const auto logger = std::make_shared<spdlog::logger>(logName, sinks.begin(), sinks.end());
+    logger->set_level(spdlog::level::trace);
+    register_logger(logger);
+}
 
 inline void InitLogger() {
     const auto consoleSink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
     consoleSink->set_level(spdlog::level::trace);
 
 #ifdef _DEBUG
-    REGISTER_FILE_LOGGER("base_logger", "base.log")
+    CreateDefaultLogger(consoleSink, "base_logger", "base.log");
 #endif
 }

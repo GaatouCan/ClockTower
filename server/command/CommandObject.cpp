8ce2869be69ba67d74cbf4a1f770c0c530054aa2
admin <xxx@xx.xom>
2024-11-15 18:30:50 +0800

    Initial commit

--- server/command/CommandObject.cpp
+++ server/command/CommandObject.cpp
@@ -1,5 +1,19 @@
-//
-// Created by admin on 24-11-15.
-//
-
 #include "CommandObject.h"
+#include "../base/utils.h"
+
+#include <utility>
+
+
+UCommandObject::UCommandObject(std::string cmdType, std::string cmdOriginParam)
+    : mCmdType(std::move(cmdType)),
+      mCmdOriginParam(std::move(cmdOriginParam)),
+      mCmdParams(SplitString(mCmdOriginParam, '|')) {
+}
+
+std::string UCommandObject::GetCommandType() const {
+    return mCmdType;
+}
+
+std::string UCommandObject::GetCommandOriginParam() const {
+    return mCmdOriginParam;
+}
--- server/command/CommandObject.h
+++ server/command/CommandObject.h
@@ -1,7 +1,21 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
 
 class UCommandObject final {
+
+    std::string mCmdType;
+    std::string mCmdOriginParam;
+    std::vector<std::string> mCmdParams;
+
+public:
+    UCommandObject() = delete;
+    explicit UCommandObject(std::string  cmdType, std::string  cmdOriginParam = "");
+
+    [[nodiscard]] std::string GetCommandType() const;
+    [[nodiscard]] std::string GetCommandOriginParam() const;
 
 };
 

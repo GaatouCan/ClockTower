608539cc9871d0a33f88a1b88918f204666e99e2
Gaatou <1752651025@qq.com>
2024-11-17 11:49:50 +0800

    Initial commit

--- server/command/CommandObject.cpp
+++ server/command/CommandObject.cpp
@@ -2,6 +2,7 @@
 #include "../base/utils.h"
 
 #include <utility>
+#include <spdlog/spdlog.h>
 
 
 UCommandObject::UCommandObject(std::string cmdType, std::string cmdOriginParam)
@@ -16,4 +17,32 @@ std::string UCommandObject::GetCommandType() const {
 
 std::string UCommandObject::GetCommandOriginParam() const {
     return mCmdOriginParam;
+}
+
+void UCommandObject::Reset() {
+    mIndex = 0;
+}
+
+int UCommandObject::ReadInt() {
+    if (mIndex >= mCmdParams.size()) {
+        spdlog::error("UCommandObject::ReadInt: index out of range");
+        return -1;
+    }
+    return std::stoi(mCmdParams[mIndex++]);
+}
+
+unsigned int UCommandObject::ReadUInt() {
+    if (mIndex >= mCmdParams.size()) {
+        spdlog::error("UCommandObject::ReadInt: index out of range");
+        return 0;
+    }
+    return std::stoul(mCmdParams[mIndex++]);
+}
+
+std::string UCommandObject::ReadString() {
+    if (mIndex >= mCmdParams.size()) {
+        spdlog::error("UCommandObject::ReadInt: index out of range");
+        return {};
+    }
+    return mCmdParams[mIndex++];
 }
--- server/command/CommandObject.h
+++ server/command/CommandObject.h
@@ -9,14 +9,20 @@ class UCommandObject final {
     std::string mCmdType;
     std::string mCmdOriginParam;
     std::vector<std::string> mCmdParams;
+    size_t mIndex;
 
 public:
     UCommandObject() = delete;
-    explicit UCommandObject(std::string  cmdType, std::string  cmdOriginParam = "");
+    explicit UCommandObject(std::string cmdType, std::string cmdOriginParam = "");
 
     [[nodiscard]] std::string GetCommandType() const;
     [[nodiscard]] std::string GetCommandOriginParam() const;
 
+    void Reset();
+
+    int ReadInt();
+    unsigned int ReadUInt();
+    std::string ReadString();
 };
 
 

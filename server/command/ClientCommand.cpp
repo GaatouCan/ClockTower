d972039a0b14a1712db0b1782bd01295a7419ab3
admin <xxx@xx.xom>
2024-11-18 20:04:25 +0800

    Initial commit

--- server/command/ClientCommand.cpp
+++ server/command/ClientCommand.cpp
@@ -1,5 +1,13 @@
 #include "ClientCommand.h"
 
 IClientCommand::IClientCommand(UCommandObject param)
-    : IAbstractCommand(std::move(param)) {
+    : IAbstractCommand(std::move(param)), mSender(0) {
+}
+
+void IClientCommand::SetSender(const uint64_t sender) {
+    mSender = sender;
+}
+
+uint64_t IClientCommand::GetSender() const {
+    return mSender;
 }
--- server/command/ClientCommand.h
+++ server/command/ClientCommand.h
@@ -5,8 +5,13 @@
 
 class IClientCommand : public IAbstractCommand {
 
+    uint64_t mSender;
+
 public:
     explicit IClientCommand(UCommandObject param);
+
+    void SetSender(uint64_t sender);
+    [[nodiscard]] uint64_t GetSender() const;
 
 };
 

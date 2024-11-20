9cac39e5bacae67816a0e44c0a32a03652ed9737
Gaatou <1752651025@qq.com>
2024-11-21 01:04:42 +0800

    Initial commit

--- base/manager/command/CommandManager.cpp
+++ base/manager/command/CommandManager.cpp
@@ -1,8 +1,27 @@
 #include "CommandManager.h"
+#include "CommandObject.h"
+
+#include "../player/AbstractPlayer.h"
 
 UCommandManager::UCommandManager(asio::io_context &ctx)
     : IManager(ctx) {
 }
 
 UCommandManager::~UCommandManager() {
+}
+
+void UCommandManager::OnClientCommand(const std::shared_ptr<IAbstractPlayer> &player, const std::string &type, const std::string &args) {
+    const auto iter = mClientCmdMap.find(type);
+    if (iter == mClientCmdMap.end()) {
+        return;
+    }
+
+    const UCommandObject obj(type, args);
+
+    if (const auto cmd = dynamic_cast<IClientCommand*>(std::invoke(iter->second, obj)); cmd != nullptr) {
+        cmd->SetSender(player->GetPlayerID());
+        cmd->Execute();
+
+        delete cmd;
+    }
 }
--- base/manager/command/CommandManager.h
+++ base/manager/command/CommandManager.h
@@ -2,8 +2,19 @@
 
 #include "../../system/manager/Manager.h"
 
+#include "ClientCommand.h"
+#include "OperateCommand.h"
+
+class IAbstractPlayer;
+class UCommandObject;
+
 
 class UCommandManager final : public IManager {
+
+    using ACommandCreator = std::function<IAbstractCommand*(const UCommandObject&)>;
+
+    std::unordered_map<std::string, ACommandCreator> mOperateCmdMap;
+    std::unordered_map<std::string, ACommandCreator> mClientCmdMap;
 
 public:
     explicit UCommandManager(asio::io_context &ctx);
@@ -12,4 +23,27 @@ public:
     [[nodiscard]] constexpr const char * GetManagerName() const override {
         return "UCommandManager";
     }
+
+    template<class T>
+    requires std::derived_from<T, IClientCommand>
+    void RegisterClientCommand(const std::string &cmd) {
+        if (!mClientCmdMap.contains(cmd)) {
+            mClientCmdMap[cmd] = [](const UCommandObject &obj) -> IAbstractCommand* {
+                return new T(obj);
+            };
+        }
+    }
+
+    template<class T>
+    requires std::derived_from<T, IOperateCommand>
+    void RegisterOperateCommand(const std::string &cmd) {
+        if (!mOperateCmdMap.contains(cmd)) {
+            mOperateCmdMap[cmd] = [](const UCommandObject &obj) -> IAbstractCommand* {
+                return new T(obj);
+            };
+        }
+    }
+
+
+    void OnClientCommand(const std::shared_ptr<IAbstractPlayer> &player, const std::string &type, const std::string &args);
 };

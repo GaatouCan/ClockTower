f20006daa95e3de91c03a36883916c801e0e4cce
admin <xxx@xx.xom>
2024-11-21 11:29:45 +0800

    Initial commit

---
+++ server/world/manager/chat/ChatRoom.cpp
@@ -0,0 +1,5 @@
+//
+// Created by admin on 24-11-21.
+//
+
+#include "ChatRoom.h"
---
+++ server/world/manager/chat/ChatRoom.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <memory>
+
+
+class UChatRoom final : public std::enable_shared_from_this<UChatRoom> {
+};

5db34a7ea9f5760bfd194ea426cbbf0391825301
Astria <1752651025@qq.com>
2024-11-26 02:03:24 +0800

    Initial commit

--- base/system/config/ConfigSystem.cpp
+++ base/system/config/ConfigSystem.cpp
@@ -54,7 +54,11 @@ void UConfigSystem::Init() {
                 strlen(jsonPath.c_str()) + 1,
                 filepath.length() - 6 - strlen(jsonPath.c_str()));
 
+#ifdef WIN32
             filepath = StringReplace(filepath, '\\', '.');
+#elifdef __linux__
+            filepath = StringReplace(filepath, '/', '.');
+#endif
 
             mJSONConfigMap[filepath] = nlohmann::json::parse(fs);
             spdlog::info("\tLoaded {}.", filepath);
